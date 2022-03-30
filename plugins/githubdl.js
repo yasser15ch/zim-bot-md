9d5a44a3d32d932cdb0cc178a518ae7f829c2213
DRIPS MEMES BLENNIE BLESSED TUNA <100164866+zim-bot@users.noreply.github.com>
2022-03-30 13:48:02 +0200

    Update githubdl.js

--- plugins/githubdl.js
+++ plugins/githubdl.js
@@ -4,7 +4,7 @@ if (!args[0]) throw 'where\'s the username ?'
 if (!args[1]) throw 'where\'s the repo?'
 
 let url = `https://github.com/${args[0]}/${args[1]}/zipball/master`
-//ByRizkyAdi
+//DripsMemes
 m.reply(`compressing data to zip file`)
 conn.sendFile( m.chat, url, 'repo.zip', null, m)
 

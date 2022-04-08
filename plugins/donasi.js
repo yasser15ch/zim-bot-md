1a1390cc6ecc38809ce890c3f31ed3a3b2d19318
DRIPS MEMES BLENNIE BLESSED TUNA <100164866+zim-bot@users.noreply.github.com>
2022-04-08 18:49:26 +0200

    ZIM BOT INC 2022-MD

--- plugins/donasi.js
+++
@@ -1,8 +0,0 @@
-let handler = async m => m.reply(`
-Drips is rich, he doesn't need your money.
-`.trim()) // Handikwerete just enjoy the bot
-handler.help = ['donate']
-handler.tags = ['info']
-handler.command = /^dona(te|si)$/i
-
-module.exports = handler

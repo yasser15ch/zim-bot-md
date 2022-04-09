b4715fff8365e80c1ffad6b0f7214b2db646d854
DRIPS MEMES BLENNIE BLESSED TUNA <100164866+zim-bot@users.noreply.github.com>
2022-04-09 20:23:01 +0200

    𝗭𝗜𝗠 𝗕𝗢𝗧 𝗜𝗡𝗖 2022-𝗠𝗗

--- plugins/youtube-play.js
+++ plugins/youtube-play.js
@@ -3,15 +3,12 @@ let handler = async (m, { conn, command, text, usedPrefix }) => {
   if (!text) throw `Use example ${usedPrefix}${command} Minecraft`
   let vid = (await youtubeSearch(text)).video[0]
   if (!vid) throw 'Video/Audio Not found'
-  let { title, description, thumbnail, videoId, durationH, viewH, publishedTime } = vid
+  let { title, description, thumbnail, videoId, durationH } = vid
   const url = 'https://www.youtube.com/watch?v=' + videoId
   await conn.sendHydrated(m.chat, `
-📌 *Title:* ${title}
-🔗 *Url:* ${url}
-🖹 *Description:* ${description}
-⏲️ *Published:* ${publishedTime}
-⌚ *Duration:* ${durationH}
-👁️ *Views:* ${viewH}
+🌎 *𝗧𝗜𝗧𝗟𝗘:* ${title}
+🌍 *𝗨𝗥𝗟:* ${url}
+🌞 *𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗧𝗜𝗢𝗡:* ${description}
   `.trim(), author, thumbnail, '', '', null, null, [
     ['SONG', `${usedPrefix}yta ${url} yes`],
     ['VIDEO', `${usedPrefix}ytv ${url} yes`]

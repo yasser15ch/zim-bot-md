8a2f426e883a74f0caf0166613a71e3c0e54cc69
DRIPS MEMES BLENNIE BLESSED TUNA <100164866+zim-bot@users.noreply.github.com>
2022-04-09 08:26:53 +0200

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -41,13 +41,8 @@ const defaultMenu = {
 ┃ ⭐ 𝗗𝗔𝗧𝗔𝗕𝗔𝗦𝗘: %rtotalreg of %totalreg
 ┗━━━━━━━━━━━━━━━━⦂
 %readmore`.trimStart(),
-  header: '╭━━━❰ %main ❱',
+  header: '╭━━━❰ %category ❱',
   body: '┃ 🌍 %cmd %islimit %isPremium',
-  footer: '╰━━━━━━━⦂\n',
-  after: `
-%readmore`.trimStart(),
-  header: '╭━━━❰ %nsfw ❱',
-  body: '┃ 🔮 %cmd %islimit %isPremium',
   footer: '╰━━━━━━━⦂\n',
   after: `
 𝐙𝐈𝐌𝐁𝐎𝐓 𝐈𝐍𝐂 |©𝐃𝐑𝐈𝐏𝐒

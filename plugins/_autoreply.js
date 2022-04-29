560a01e3a40b0e592939c4cad1725c61f4295cf5
DRIPS MEMES BLENNIE BLESSED TUNA <100164866+zim-bot@users.noreply.github.com>
2022-04-29 13:50:54 +0200

    Create _autoreply.js

---
+++ plugins/_autoreply.js
@@ -0,0 +1,22 @@
+import fs from "fs"
+let handler = m => m
+
+handler.all = async function (m) {
+let vn = './media/bot.mp3'
+const estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) },
+message: { 
+orderMessage: { itemCount : -27634090203, status: 1, surface : 1, message: 'ZIMBOT INC 2022', orderTitle: 'Bang', thumbnail: fs.readFileSync('./Menu2.jpg'), sellerJid: '0@s.whatsapp.net'    
+}}}
+const estiloaudio = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) },
+message: { 
+"audioMessage": { "mimetype":"audio/ogg; codecs=opus", "seconds": "99569", "ptt": "true"   
+}}}  
+
+if (/^bot$/i.test(m.text)) {
+    
+conn.sendButton(m.chat, '*HI MY LOVELY CHOMIE WELCOME ZIM BOT PRESENTS*', wm, [['MENU', `#menu`]], 'conversation', { sendEphemeral: true, quoted: estilo })
+conn.sendFile(m.chat, vn, 'bot.mp3', null, m, true, { type: 'audioMessage', ptt: true, sendEphemeral: true, quoted: estiloaudio })   
+}
+return !0
+}
+export default handler
